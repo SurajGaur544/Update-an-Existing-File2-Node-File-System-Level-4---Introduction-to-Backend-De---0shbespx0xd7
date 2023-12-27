2fd181bc8c6cca6e363ce3ee65975e3b6b64ef1c
Suraj Gaur <surajgaur242@gmail.com>
2023-12-28 00:34:27 +0530

    Add some specific code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
     
+    fs.appendFile(fileName,fileContent, function(err) {
+        if(err) throw err;
+        console.log("File Updated");
+    });
+    
+    
 };
-
-
+updateFile(fileName,"Newton School, is an online learning platform.");
 module.exports = updateFile;
